7198cc36c53218f379e2e86822a3770659572c65
zs-pad <someone000@126.com>
2018-10-02 00:23:24 +0800

    Add project files.

---
+++ asyncTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

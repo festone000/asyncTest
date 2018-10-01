ffc1a113bd8e01c435d2bc9884098cd7be4d4a69
zs-pad <someone000@126.com>
2018-10-02 00:36:54 +0800

    Task 简单使用。

--- asyncTest/Program.cs
+++ asyncTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace asyncTest
 {
@@ -6,7 +8,27 @@ namespace asyncTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Task with Thread Start!");
+
+            for (var i = 0; i <= 5; i++)
+            {
+                Thread t = new Thread(DoTaskFunction);
+                t.Start();
+            }
+
+            Console.WriteLine("Task with Thread End!");
+
+            for(var i = 0; i <= 5; i++)
+            {
+                Task.Run(()=> { DoTaskFunction(); });
+            }
+            Console.WriteLine("Task with task end");
+
+        }
+
+        public static void DoTaskFunction()
+        {
+            Console.WriteLine($" ThreadId {Thread.CurrentThread.ManagedThreadId}, \t  IsBackGround {Thread.CurrentThread.IsBackground}");
         }
     }
 }

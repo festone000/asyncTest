42ba3e9fd4ef0d7ab95dfd07e932e4f11fcdecee
zs-pad <someone000@126.com>
2018-10-02 00:54:31 +0800

    C#  Thread ThreadPool Task Await Async 示例。

--- asyncTest/Program.cs
+++ asyncTest/Program.cs
@@ -8,21 +8,24 @@ namespace asyncTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task with Thread Start!");
+            //Console.WriteLine("Task with Thread Start!");
 
-            for (var i = 0; i <= 5; i++)
-            {
-                Thread t = new Thread(DoTaskFunction);
-                t.Start();
-            }
+            //for (var i = 0; i <= 5; i++)
+            //{
+            //    Thread t = new Thread(DoTaskFunction);
+            //    t.Start();
+            //}
 
-            Console.WriteLine("Task with Thread End!");
+            //Console.WriteLine("Task with Thread End!");
 
-            for(var i = 0; i <= 5; i++)
-            {
-                Task.Run(()=> { DoTaskFunction(); });
-            }
-            Console.WriteLine("Task with task end");
+            //for (var i = 0; i <= 5; i++)
+            //{
+            //    Task.Run(() => { DoTaskFunction(); });
+            //}
+            //Console.WriteLine("Task with task end");
+
+            //DoTaskWithAsync();
+            Test2();
 
         }
 
@@ -30,5 +33,50 @@ namespace asyncTest
         {
             Console.WriteLine($" ThreadId {Thread.CurrentThread.ManagedThreadId}, \t  IsBackGround {Thread.CurrentThread.IsBackground}");
         }
+
+        #region 常规Task
+        public static async void DoTaskWithAsync()
+        {
+            Console.WriteLine("Await Taskfunction Start");
+            await Task.Run(() => { DoTaskFunction2(); });
+        }
+
+
+        public static void  DoTaskFunction2()
+        {
+            for (var i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("task {0}", i);
+                //ThreadPool.QueueUserWorkItem((Object obj)=> {
+                //    Console.WriteLine(i);
+                //});
+            }
+        }
+
+        #endregion
+        //-------------
+
+        #region 泛型Task，跟Task返回值有关
+        public static async void Test2()
+        {
+            Console.WriteLine("========");
+            var a = await Task<string>.Run<string>(() => {
+                var aaa = Test222();
+                Console.WriteLine(aaa);
+                return aaa;
+            });
+            Console.WriteLine("~~~~" + a);
+        }
+
+        public static string Test222()
+        {
+            Random random = new Random();
+            var v = random.Next(1000, 10000);
+            return v.ToString();
+        }
+
+
+        #endregion
+        
     }
 }
